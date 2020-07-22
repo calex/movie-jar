9694f511fc7a238fc58b77e575704dfbcdefb312
Chris.Alexander <chris.alexander@mac.com>
2020-07-21 22:34:04 -0500

    Complete app cycle working with semi-randomized film selection.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,28 @@
 'use strict';
 
-const STORE = [];
+// Helpers
+const showDomItem = ($item) => {
+    $item.classList.remove('hidden');
+}
 
-function formatQueryParams(params) {
+const hideDomItem = ($item) => {
+    $item.classList.add('hidden');
+}
+
+const getRandomItemFromArray = (array) => {
+    return array[Math.floor(Math.random() * array.length)];   
+}
+
+const formatQueryParams = (params) => {
     const queryItems = Object.keys(params)
       .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
 }
 
+// Global initializers
+const STORE = [];
+
+// Store populator functions
 function getFilmsExpiringSoon() {
     fetch("https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Aexp%3AUS&t=ns&st=adv&p=1&type=movie", {
         "method": "GET",
@@ -28,7 +43,7 @@ function getFilmsExpiringSoon() {
     });
 }
 
-function addGenreAndMovieToStore(genreName, movieObjectToStore) {
+function checkIfGenreRecordExistsThenAdd(genreName, movieObjectToStore) {
     const genreAlreadyInStore = STORE.some(el => el.genreName === genreName);
 
     if (!genreAlreadyInStore) {
@@ -55,7 +70,8 @@ function addRecordToStoreByGenres(genres, movie) {
         movieObjectToStore.id = movie.nfinfo.netflixid;
         movieObjectToStore.info = movie;
         
-        genres.forEach(genreName => addGenreAndMovieToStore(genreName, movieObjectToStore));
+        genres.forEach(genreName => checkIfGenreRecordExistsThenAdd(genreName, movieObjectToStore));
+        console.log('STORE: ', STORE);
     }
 }
 
@@ -79,6 +95,72 @@ function getSingleFilmInfo(netflixId) {
     });
 }
 
+// UI functions
+const watchForms = () => {
+    const $genreForm = document.querySelector('#js-genre-choice-form');
+    const $movieChoiceForm = document.querySelector('#js-movie-choice-form');
+    const $movieJarSection = document.querySelector('#js-movie-jar');
+    const $movieSelectionSection = document.querySelector('#js-movie-selection-showcase');
+    
+    let RANDOM_GENRE = "";
+    let RANDOM_FILM_WITHIN_GENRE = "";
+
+    $genreForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const randomStoreItem = getRandomItemFromArray(STORE);
+
+        RANDOM_GENRE = randomStoreItem.genreName;
+
+        RANDOM_FILM_WITHIN_GENRE = getRandomItemFromArray(randomStoreItem.moviesInGenre);
+
+        $movieChoiceForm.innerHTML = `<h3>You drew <b>${RANDOM_GENRE}</b><h3><button type='submit'>Get a movie in this genre</button>`;
+
+        hideDomItem($genreForm);
+        showDomItem($movieChoiceForm);     
+    });
+
+    $movieChoiceForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        $movieSelectionSection.innerHTML = 
+            `
+                <img class='movie-showcase__poster' src=${RANDOM_FILM_WITHIN_GENRE.info.nfinfo.image1}>
+                <div class='movie-showcase__summary'>
+                    <h3>${RANDOM_FILM_WITHIN_GENRE.info.nfinfo.title} (${RANDOM_FILM_WITHIN_GENRE.info.nfinfo.released})</h3>
+                    <h5>
+                        <span>Runtime: ${RANDOM_FILM_WITHIN_GENRE.info.imdbinfo.runtime}</span>
+                        &middot;
+                        <span>IMDB Rating: ${RANDOM_FILM_WITHIN_GENRE.info.imdbinfo.rating} / 10</span>
+                    </h5>
+                    <p>${RANDOM_FILM_WITHIN_GENRE.info.nfinfo.synopsis}</p>
+                    <button id='js-start-over-button' type='submit' class='start-over-button'>Start Over</button>
+                </div>
+            `;
+
+        hideDomItem($movieJarSection);
+        showDomItem($movieSelectionSection);
+
+        const $startOverButton = $movieSelectionSection.querySelector('#js-start-over-button');
+
+        $startOverButton.addEventListener("click", (e) => {
+            RANDOM_GENRE = "";
+            RANDOM_FILM_WITHIN_GENRE = "";
+            
+            $movieChoiceForm.innerHTML = '';
+            $movieSelectionSection.innerHTML = '';
+
+            hideDomItem($movieChoiceForm);
+            hideDomItem($movieSelectionSection);
+
+            showDomItem($movieJarSection);
+            showDomItem($genreForm);
+        });
+    });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     getFilmsExpiringSoon();
+    watchForms();
 });
+
